c80c7fab9a85b9340fc14a1c4d6ee47c3bd9de67
José Henrique <135862618+josehlopes@users.noreply.github.com>
2024-09-09 23:37:46 -0300

    :bug: FIX: UserService was Duplicate

--- src/main/java/com/thigas/quack/adapter/controller/UserController.java
+++ src/main/java/com/thigas/quack/adapter/controller/UserController.java
@@ -1,11 +1,19 @@
-package com.thigas.quack.presentation.controller;
-
-import com.thigas.quack.domain.entity.User;
-import com.thigas.quack.application.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+package com.thigas.quack.adapter.controller;
 
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thigas.quack.application.service.UserService;
+import com.thigas.quack.domain.entity.User;
 
 @RestController
 @RequestMapping("/users")
