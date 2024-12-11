27b430836330c7f02b6525093c075c80c6593fb1
José Henrique Silva Lopes <josehenrique.s2002@hotmail.com>
2024-12-10 23:22:27 -0300

    🔧 FIX: Fix double routes

--- src/main/java/com/thigas/quack/Adapter/Controller/UserController.java
+++ src/main/java/com/thigas/quack/Adapter/Controller/UserController.java
@@ -69,24 +69,5 @@ public class UserController {
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-<<<<<<< Updated upstream
-
-    @PutMapping("/deactivate/{id}")
-    public ResponseEntity<Void> deactivateUser(@PathVariable Integer id) {
-        try {
-            ResponseWrapper<GenericResponseModel> response = userInput.deactivateUser(id);
-            if (response.getStatusCode() == 200) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            } else {
-                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        } catch (NoSuchElementException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-=======
->>>>>>> Stashed changes
     }
-
 }
