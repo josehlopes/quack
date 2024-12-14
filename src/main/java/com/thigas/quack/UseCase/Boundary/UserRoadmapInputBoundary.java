16d591600592159f6d688b8936d1912cc28ce16c
ThiagoMachado2 <136006071+ThiagoMachado2@users.noreply.github.com>
2024-12-13 23:34:47 -0300

    ✨ FEAT:  getUserRoadmapsByUserId

--- src/main/java/com/thigas/quack/UseCase/Boundary/UserRoadmapInputBoundary.java
+++ src/main/java/com/thigas/quack/UseCase/Boundary/UserRoadmapInputBoundary.java
@@ -1,11 +1,15 @@
 package com.thigas.quack.UseCase.Boundary;
 
 import com.thigas.quack.UseCase.Model.Request.CompleteRoadmapRequestModel;
+import com.thigas.quack.UseCase.Model.Request.RoadmapRequestModel;
 import com.thigas.quack.UseCase.Model.Request.StartRoadmapRequestModel;
 import com.thigas.quack.UseCase.Model.Response.GenericResponseModel;
 import com.thigas.quack.UseCase.Util.ResponseWrapper;
 
+import java.util.List;
+
 public interface UserRoadmapInputBoundary {
     ResponseWrapper<GenericResponseModel> startRoadmap(StartRoadmapRequestModel request);
     ResponseWrapper<GenericResponseModel> completeRoadmap(CompleteRoadmapRequestModel request);
+    ResponseWrapper<List<RoadmapRequestModel>> getUserRoadmapsByUserId(Integer userId);
 }
--- src/main/java/com/thigas/quack/UseCase/Model/Request/RoadmapRequestModel.java
+++ src/main/java/com/thigas/quack/UseCase/Model/Request/RoadmapRequestModel.java
@@ -1,9 +1,6 @@
 package com.thigas.quack.UseCase.Model.Request;
 
-import com.thigas.quack.Domain.Utils.Status;
-
 import java.util.Set;
-
 
 public record RoadmapRequestModel(Integer id, String title, String description, String imagePath,
                                   Set<Integer> stepsId, String category) {
