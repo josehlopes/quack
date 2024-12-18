3e42d5e0adba532701dd47de5e40e93b44f66290
ThiagoMachado2 <136006071+ThiagoMachado2@users.noreply.github.com>
2024-12-18 02:24:11 -0300

    ✨ FEAT: Add-Title-In-StepRequestModel

--- src/main/java/com/thigas/quack/UseCase/Model/Response/StepInfoResponseModel.java
+++ src/main/java/com/thigas/quack/UseCase/Model/Response/StepInfoResponseModel.java
@@ -5,5 +5,5 @@ import com.thigas.quack.Domain.Utils.Status;
 import java.util.Set;
 
 public record StepInfoResponseModel(Integer id, Set<Integer> roadmapsIds, Set<Integer> lessonsIds, Set<Integer> tasksIds,
-                                    String description, String imagePath, Status status) {
+                                    String description, String imagePath, Status status, String title) {
 }
